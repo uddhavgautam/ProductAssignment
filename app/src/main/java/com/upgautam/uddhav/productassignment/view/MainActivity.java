0cec857a2fe01d62b98af46d9c99126280ccc752
uddhavgautam <mynepal12AS$$>
2017-11-12 22:53:05 -0500

    first update @uddhav

--- app/src/main/java/com/upgautam/uddhav/productassignment/view/MainActivity.java
+++ app/src/main/java/com/upgautam/uddhav/productassignment/view/MainActivity.java
@@ -1,15 +1,41 @@
 package com.upgautam.uddhav.productassignment.view;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.upgautam.uddhav.productassignment.R;
+import com.upgautam.uddhav.productassignment.model.productpackage.Product;
+
+
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Add product list fragment if this is first creation
+        if (savedInstanceState == null) {
+            ProductListFragment fragment = new ProductListFragment();
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, fragment, ProductListFragment.TAG).commit();
+        }
+    }
+
+    /**
+     * Shows the product detail fragment
+     */
+    public void show(Product product) {
+
+        ProductFragment productFragment = ProductFragment.forProduct(product.getmId());
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .addToBackStack("product")
+                .replace(R.id.fragment_container,
+                        productFragment, null).commit();
     }
 }
+
--- app/src/main/java/com/upgautam/uddhav/productassignment/view/ProductListFragment.java
+++ app/src/main/java/com/upgautam/uddhav/productassignment/view/ProductListFragment.java
@@ -1,8 +1,12 @@
 package com.upgautam.uddhav.productassignment.view;
 
+import android.support.v4.app.Fragment;
+
 /**
  * Created by Administrator on 11/11/2017.
  */
 
-public class ProductListFragment {
+public class ProductListFragment extends Fragment {
+    public static final String TAG = "ProductListViewModel";
+
 }
